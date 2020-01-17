139dc37b71d0e1d96ce7d97244b606ca7f9cd86c
Johnny Hay <johnny.hay@ed.ac.uk>
2020-01-17 15:20:19 +0000

    Update setup.py version to 1.0.0a

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='v0.9.1a',  # Required
+    version='1.0.0a',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
