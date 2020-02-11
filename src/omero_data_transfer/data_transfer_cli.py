f4f3577049ba5b8791050607eb431f4a09119e11
Johnny Hay <j.hay@epcc.ed.ac.uk>
2020-02-11 09:31:47 +0000

    Fix input args

--- src/omero_data_transfer/data_transfer_cli.py
+++ src/omero_data_transfer/data_transfer_cli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import argparse
 
@@ -6,26 +8,46 @@ parser = argparse.ArgumentParser(description='PyOmeroUpload Data Transfer Applic
 
 # mandatory args
 parser.add_argument('-d', '--data-path', dest='data_path',
-    type=string, required=True, action='store_true', metavar='data-path',
+    type=str, required=True, metavar='data-path',
     help="specifies the system file path to the data directory for uploading")
 
 parser.add_argument('-n', '--dataset-name', dest='dataset_name',
-    type=string, required=True, action='store_true', metavar='dataset-name',
+    type=str, required=True, metavar='dataset-name',
     help="specifies the name of the destination dataset")
 
 # optional args
-parser.add_argument('-h', '--hypercube', dest='format',
-    type=bool, required=False, action='store_true',  metavar='hypercube',
+parser.add_argument('-c', '--hypercube', dest='hypercube',
+    type=bool, required=False, metavar='hypercube',
     help="commands the uploader to generate hypercube images")
 
-parser.add_argument('-m', '--module-path', dest='format',
-    type=string, required=False, action='store_true', metavar='module-path',
+parser.add_argument('-m', '--module-path', dest='module_path',
+    type=str, required=False, metavar='module-path',
     help="specifies the system file path to the directory containing custom classes")
 
-parser.add_argument('-p', '--parser-class', dest='format',
-    type=string, required=False, action='store_true', metavar='arser-class',
+parser.add_argument('-p', '--parser-class', dest='parser_class',
+    type=str, required=False, metavar='parser-class',
     help="specifies the name of the custom metadata parser class")
 
-parser.add_argument('-i', '--image-processor-class', dest='format',
-    type=string, required=False, action='store_true', metavar='image-processor-class',
+parser.add_argument('-i', '--image-processor-class', dest='image_processor_class',
+    type=str, required=False, metavar='image-processor-class',
     help="specifies the name of the custom image processor class")
+
+args = parser.parse_args()
+
+if args.data_path:
+    print args.data_path
+
+if args.dataset_name:
+    print args.dataset_name
+
+if args.hypercube:
+    print args.hypercube
+
+if args.module_path:
+    print args.module_path
+
+if args.parser_class:
+    print args.parser_class
+
+if args.image_processor_class:
+    print args.image_processor_class
