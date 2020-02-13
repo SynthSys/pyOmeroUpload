18cc41996acf1e199fc3334664570266557f5ac4
Johnny Hay <j.hay@epcc.ed.ac.uk>
2020-02-13 15:55:40 +0000

    Using OMERO CLI cli class for importing/uploading original image files - needs rationalisation

--- src/omero_data_transfer/omero_data_broker.py
+++ src/omero_data_transfer/omero_data_broker.py
@@ -36,6 +36,34 @@ class OMERODataType(Enum):
     def __str__(self):
         return self.name.capitalize()
 
+from omero import java as om_java
+import subprocess
+
+def popen(args,
+          java="/usr/local/openjdk-8/bin/java",
+          #java="java",
+          # xargs=None,
+          xargs="-classpath /home/jovyan/work/OMERO.server-5.4.10-ice36-b105/lib/server/*",
+          chdir=None,
+          debug=None,
+          debug_string=om_java.DEFAULT_DEBUG,
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE):
+    """
+    Creates a subprocess.Popen object and returns it. Uses cmd() internally
+    to create the Java command to be executed. This is the same logic as
+    run(use_exec=False) but the Popen is returned rather than the stdout.
+    """
+    from omero import java as om_java
+    print args
+    command = om_java.cmd(args, java, xargs, chdir, debug, debug_string)
+    print args
+    om_java.check_java(command)
+    if not chdir:
+        chdir = os.getcwd()
+    return subprocess.Popen(command, stdout=stdout, stderr=stderr,
+                            cwd=chdir, env=os.environ)
+
 
 class OMERODataBroker:
 
@@ -49,6 +77,10 @@ class OMERODataBroker:
     LINKED_ANNOS_BY_DS_QUERY = "select d from Dataset d left outer join " \
                                  "fetch d.annotationLinks as links left outer join fetch " \
                                  "links.child as annotation where d.id=:did"
+
+    ACCEPTED_MIME_TYPES = ['image/jpeg', 'image/jpx', 'image/png', 'image/gif', 'image/webp', 'image/x-canon-cr2',
+                           'image/tiff', 'image/bmp', 'image/vnd.ms-photo', 'image/vnd.adobe.photoshop', 'image/x-icon',
+                           'image/heic']
 
     def __init__(self, username="", password="", host="",
                  port=0, image_processor=DefaultImageProcessor()):
@@ -212,20 +244,119 @@ class OMERODataBroker:
 
         return table
 
+    import subprocess
+    from omero import java as om_java
+
     def upload_image(self, file_to_upload, dataset):
-        # convert image to 2DArray for plane
-        im = Image.open(file_to_upload)
+        valid_image = False
+        file_mime_type = None
 
-        imarray = np.array(im)
-        print imarray
+        if file_to_upload.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff')):
+            import filetype
+            ftype = filetype.guess(file_to_upload)
+            if ftype is None:
+                print('Cannot guess file type!')
+                valid_image = False
 
-        filename_w_ext = os.path.basename(file_to_upload)
-        filename, file_extension = os.path.splitext(filename_w_ext)
-        print filename_w_ext
+            print('File extension: %s' % ftype.extension)
+            print('File MIME type: %s' % ftype.mime)
 
-        script_utils.createNewImage(self.SESSION, [imarray], filename,
-                                    "An image", dataset)
+            if ftype.mime not in self.ACCEPTED_MIME_TYPES:
+                valid_image = False
+            else:
+                valid_image = True
+                file_mime_type = ftype.mime
+        else:
+            valid_image = False
 
+        if valid_image == True:
+            # convert image to 2DArray for plane
+            im = Image.open(file_to_upload)
+            # planes = [np.array(im)]
+
+            filename_w_ext = os.path.basename(file_to_upload)
+            filename, file_extension = os.path.splitext(filename_w_ext)
+            print filename_w_ext
+
+            planes = script_utils.getPlaneFromImage(imagePath = file_to_upload, rgbIndex=None)
+            print planes
+            #script_utils.createNewImage(self.SESSION, [planes], filename,
+            #                            "An image", dataset)
+
+            query_service = self.SESSION.getQueryService()
+            update_service = self.SESSION.getUpdateService()
+            raw_file_store = self.SESSION.createRawFileStore()
+
+            tempFile = model.OriginalFileI()
+            tempFile.setName(rtypes.rstring(filename))
+            tempFile.setPath(rtypes.rstring(filename))
+            tempFile.setMimetype(rtypes.rstring(file_mime_type))
+            tempFile.setSize(rtypes.rlong(len(np.array(im))))
+            tempFile.setHash(rtypes.rstring(script_utils.calcSha1FromData(np.array(im))))
+            original_file = update_service.saveAndReturnObject(tempFile)
+            #print original_file
+
+            #link = model.JobOriginalFileLinkI()
+            #link.parent = model.DatasetI(dataset.id, False)
+
+            #link.child = model.OriginalFileI(original_file.id, False)
+            #link = update_service.saveAndReturnObject(link)
+            #print link
+            from omero import gateway
+            conn = gateway.BlitzGateway(client_obj=self.CLIENT)
+
+            original_file = conn.createOriginalFileFromLocalFile(localPath = file_to_upload,
+                                        origFilePathAndName = filename_w_ext,
+                                        mimetype=file_mime_type, ns=None)
+            # print original_file._obj
+            '''
+            path, name = os.path.split(file_to_upload)
+            fileSize = os.path.getsize(file_to_upload)
+            with open(file_to_upload, 'rb') as fileHandle:
+                return self.createOriginalFileFromFileObj(fileHandle, path, name,
+                                                          fileSize, file_mime_type)
+            '''
+            #OriginalFileWrapper
+            # link = model.DatasetOriginalFileLinkI()
+
+            from omero import cli
+            cli = cli.CLI()
+            cli.loadplugins()
+            cli.set_client(self.CLIENT)
+
+            os.environ["JAVA_HOME"] = "/usr/local/openjdk-8"
+            os.environ["PATH"] = "$PATH:$JAVA_HOME/bin"
+
+            from omero import java
+            java.popen = popen
+            java.check_java(["/usr/local/openjdk-8/bin/java"])
+            print "found java"
+            #command = java.cmd(args, java, xargs, chdir, debug, debug_string)
+            # check_java(command)
+            if dataset:
+                target = "Dataset:id:" + str(dataset.id.getValue())
+                cli.onecmd(["import", "--clientdir", "/home/jovyan/work/OMERO.server-5.4.10-ice36-b105/lib/client",
+                            '-T', target, '--description', "an image",
+                            '--no-upgrade-check', file_to_upload])
+            else:
+                cli.onecmd(["import", "--clientdir", "/home/jovyan/work/OMERO.server-5.4.10-ice36-b105/lib/client",
+                            '--description', "an image", '--no-upgrade-check', file_to_upload])
+
+            '''
+            link = model.DatasetImageLinkI()
+            link.parent = model.DatasetI(dataset.id, False)
+            print rtypes.rlong(original_file._obj.id)
+            remote_image = conn.getObject("Image", original_file._obj.id)
+            print remote_image
+            #link.child = model.OriginalFileI(rtypes.rlong(original_file._obj.id), False)
+            link.child = model.ImageI(original_file._obj.id, False)
+            link = update_service.saveAndReturnObject(link)
+            '''
+            #original_file = script_utils.uploadAndAttachFile(queryService=query_service, updateService=update_service,
+            #                                 rawFileStore=raw_file_store, parent=dataset,
+            #                                 localName=file_to_upload, mimetype=file_mime_type, description="An image",
+            #                                 namespace=None, origFilePathName=None)
+            #print original_file
 
     def upload_images(self, files_to_upload, dataset_id=None, hypercube=False):
         dataset = None
@@ -353,6 +484,74 @@ class OMERODataBroker:
 
         link.child = model.MapAnnotationI(map_anno.id, False)
         update_service.saveAndReturnObject(link)
+
+    # adapted from omero.gateway._BlitzGateway function
+    def createOriginalFileFromFileObj(self, fo, path, name, fileSize,
+                                      mimetype=None):
+        """
+        Creates a :class:`OriginalFileWrapper` from a local file.
+        File is uploaded to create an omero.model.OriginalFileI.
+        Returns a new :class:`OriginalFileWrapper`
+
+        :param conn:                    Blitz connection
+        :param fo:                      The file object
+        :param path:                    The file path
+        :param name:                    The file name
+        :param fileSize:                The file size
+        :param mimetype:                The mimetype of the file. String.
+                                        E.g. 'text/plain'
+        :return:                        New :class:`OriginalFileWrapper`
+        """
+        updateService = self.SESSION.getUpdateService()
+        # create original file, set name, path, mimetype
+        originalFile = model.OriginalFileI()
+        originalFile.setName(rtypes.rstring(name))
+        originalFile.setPath(rtypes.rstring(path))
+        if mimetype:
+            originalFile.mimetype = rtypes.rstring(mimetype)
+        originalFile.setSize(rtypes.rlong(fileSize))
+        # set sha1
+        try:
+            import hashlib
+            hash_sha1 = hashlib.sha1
+        except:
+            import sha
+            hash_sha1 = sha.new
+
+        from omero import gateway
+        conn = gateway.BlitzGateway(client_obj=self.CLIENT)
+        #conn.SERVICE_OPTS.setOmeroGroup(-1)
+        print conn.SERVICE_OPTS
+
+        fo.seek(0)
+        h = hash_sha1()
+        h.update(fo.read())
+        shaHast = h.hexdigest()
+        originalFile.setHash(rtypes.rstring(shaHast))
+        originalFile = updateService.saveAndReturnObject(
+            originalFile, self.SESSION.SERVICE_OPTS)
+
+        # upload file
+        fo.seek(0)
+        rawFileStore = self.SESSION.createRawFileStore()
+        try:
+            rawFileStore.setFileId(
+                originalFile.getId().getValue(), self.SESSION.SERVICE_OPTS)
+            buf = 10000
+            for pos in range(0, long(fileSize), buf):
+                block = None
+                if fileSize-pos < buf:
+                    blockSize = fileSize-pos
+                else:
+                    blockSize = buf
+                fo.seek(pos)
+                block = fo.read(blockSize)
+                rawFileStore.write(block, pos, blockSize, self.SESSION.SERVICE_OPTS)
+            originalFile = rawFileStore.save(self.SESSION.SERVICE_OPTS)
+        finally:
+            rawFileStore.close()
+        # return OriginalFileWrapper(self, originalFile)
+        return originalFile
 
 
 def main():
