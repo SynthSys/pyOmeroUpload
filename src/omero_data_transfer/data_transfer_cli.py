6a5d7f74cd426f8eb5b4f662062b398e5bdc90d1
Johnny Hay <j.hay@epcc.ed.ac.uk>
2020-02-25 10:03:43 +0000

    add better comments in data_transfer_cli

--- src/omero_data_transfer/data_transfer_cli.py
+++ src/omero_data_transfer/data_transfer_cli.py
@@ -163,13 +163,15 @@ if data_path is not None and dataset_name is not None:
             cls = getattr(import_module('custom_image_processor'), 'CustomImageProcessor')
             image_processor_impl = cls
 
-    # override these for custom implementations
+    # override `parser_class` for custom metadata extractor implementations
     if parser_class is None:
         from omero_metadata_parser.aggregate_metadata import MetadataAggregator as parser_class
 
+    # override `image_processor_impl` for custom image processing implementations
     if image_processor_impl is None:
         from omero_data_transfer.default_image_processor import DefaultImageProcessor as image_processor_impl
 
+    # initialise the uploader client and data transfer manager, then start upload process
     launch_upload(dataset_name=dataset_name, data_path=data_path, username=USERNAME,\
         password=PASSWORD, server=HOST, port=PORT, hypercube=hypercube,\
         parser_class=parser_class, image_processor_impl=image_processor_impl)
