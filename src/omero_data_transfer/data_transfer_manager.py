5733de855831efae878557d9f7b378bd624d9dff
Johnny Hay <johnny.hay@ed.ac.uk>
2020-02-26 16:03:45 +0000

    Remove sys.path import in data_transfer_manager

--- src/omero_data_transfer/data_transfer_manager.py
+++ src/omero_data_transfer/data_transfer_manager.py
@@ -5,10 +5,6 @@ __author__ = "Johnny Hay"
 __copyright__ = "BioRDM"
 __license__ = "mit"
 
-# override installed pyOmeroUpload package
-import sys
-# sys.path.insert(1, '/home/jovyan/work/pyOmeroUpload/src')
-
 import os
 from omero_data_transfer.omero_data_broker import OMERODataBroker
 import subprocess
